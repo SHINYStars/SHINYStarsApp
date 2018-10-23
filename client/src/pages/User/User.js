1321f4c76c5aba732e0d2e6f1c2f792020cfc662
Extrasy <luisalexpm@gmail.com>
2018-10-22 19:42:56 -0700

    user progress

---
+++ client/src/pages/User/User.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Volunteer extends Component {
+    render() {
+      return (
+        <div className="container" id="user">
+        <form>
+        </form>
+      </div>
+        );
+    }
+  }
+  
+  export default Volunteer;
--- server/models/seed.js
+++ server/models/seed.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-const Product = require('../models/product');
+// const Product = require('../models/product');
+
+
+const User = require("./user/user")
 
 // Below is an example of a method that seeds your database
 // Customize it to fit your needs, and execute it by calling it on a custom route
 // Make sure to create your schemas and plug them in - this code won't work!
-exports.seedDb = () => {
+/* exports.seedDb = () => {
     try {
         Product.insertMany( [
            { _id: new mongoose.Types.ObjectId(), item: "card", price: Math.floor(Math.random()*50), qty: 15, desc: 'you will love sending your mail with this'  },
@@ -13,6 +16,21 @@ exports.seedDb = () => {
            { _id: new mongoose.Types.ObjectId(), item: "notepad" , price: Math.floor(Math.random()*50), qty: 30, desc: 'Jot to your hearts content' },
            { _id: new mongoose.Types.ObjectId(), item: "bubble wrap", price: Math.floor(Math.random()*50), qty: 30, desc: '*Pop*' }
         ]).then((docs) => console.log('Success!', docs))
+     } catch (e) {
+        console.log (e);
+     }
+
+} */
+
+exports.seedDb = () => {
+    try {
+        Product.insertMany( [
+           { _id: new mongoose.Types.ObjectId(),  _id: "id", firstName: "first name", lastName: "last name", password: "passs", phonenumber1: "123-456-7890", phonenumber2: "123-456-7890", emailaddress: "ex@mple.com" },
+           { _id: new mongoose.Types.ObjectId(),  _id: "id", firstName: "first name", lastName: "last name", password: "passs", phonenumber1: "123-456-7890", phonenumber2: "123-456-7890", emailaddress: "ex@mple.com" },
+           { _id: new mongoose.Types.ObjectId(),  _id: "id", firstName: "first name", lastName: "last name", password: "passs", phonenumber1: "123-456-7890", phonenumber2: "123-456-7890", emailaddress: "ex@mple.com" },
+           { _id: new mongoose.Types.ObjectId(),  _id: "id", firstName: "first name", lastName: "last name", password: "passs", phonenumber1: "123-456-7890", phonenumber2: "123-456-7890", emailaddress: "ex@mple.com" },
+           { _id: new mongoose.Types.ObjectId(),  _id: "id", firstName: "first name", lastName: "last name", password: "passs", phonenumber1: "123-456-7890", phonenumber2: "123-456-7890", emailaddress: "ex@mple.com" }
+        ]).then((docs) => console.log('users added!', docs))
      } catch (e) {
         console.log (e);
      }
