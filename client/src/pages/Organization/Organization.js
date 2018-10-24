be649bb41bc96ac6e8ed70b09bc979db47988f9f
b4ruch <12469509+b4ruch@users.noreply.github.com>
2018-10-24 15:43:32 -0700

    implements sweetalert on charity registration v1

--- client/src/pages/Organization/Organization.js
+++ client/src/pages/Organization/Organization.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Input, FormBtn } from "../../components/Form";
 import API from "../../util/API";
+import SweetAlert from 'sweetalert-react';
+import 'sweetalert/dist/sweetalert.css';
 
 class Organization extends Component {
     state = {
@@ -14,7 +16,18 @@ class Organization extends Component {
         zip: "",
         phoneNumber: "",
         email: "",
+        confirmSignUp: false
     };
+
+    // componentDidMount() {
+    //     this.confirmRegistration();
+    // }
+
+    confirmRegistration = () => {
+        this.setState({
+            confirmSignUp: true
+        })
+    }
 
     handleInputChange = event => {
         const { name, value } = event.target;
@@ -23,69 +36,112 @@ class Organization extends Component {
         });
     };
 
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const { orgName, website, address1, address2, city, state, country, zip, phoneNumber, email} = this.state;
+
+        if (orgName &&
+            address1 &&
+            city &&
+            state &&
+            country &&
+            zip &&
+            phoneNumber &&
+            email) {
+            API.newOrganization({
+                orgName: orgName,
+                website: website,
+                address1: address1,
+                address2: address2,
+                city: city,
+                state: state,
+                country: country,
+                zip: zip,
+                phoneNumber: phoneNumber,
+                email: email
+            })
+                .then(res => this.confirmRegistration())
+                .catch(err => console.log(err));
+        }
+    };
+
     render() {
         return (
             <div className="container" id="organization">
                 <form>
                     <Input
                         value={this.state.orgName}
-                        onChange={this.handleInputChange} 
-                        name="orgName" 
+                        onChange={this.handleInputChange}
+                        name="orgName"
                         placeholder="Organization Name*" />
-                    <Input 
+                    <Input
                         value={this.state.website}
-                        onChange={this.handleInputChange} 
-                        name="website" 
+                        onChange={this.handleInputChange}
+                        name="website"
                         placeholder="Website*" />
-                    <Input 
+                    <Input
                         value={this.state.address1}
-                        onChange={this.handleInputChange}                         
-                        name="address1" 
+                        onChange={this.handleInputChange}
+                        name="address1"
                         placeholder="Address 1*" />
-                    <Input 
+                    <Input
                         value={this.state.address2}
-                        onChange={this.handleInputChange}                         
-                        name="address2" 
+                        onChange={this.handleInputChange}
+                        name="address2"
                         placeholder="Address 2" />
                     <Input
                         value={this.state.city}
-                        onChange={this.handleInputChange}                      
-                        name="city" 
+                        onChange={this.handleInputChange}
+                        name="city"
                         placeholder="City*" />
-                    <Input 
+                    <Input
                         value={this.state.state}
-                        onChange={this.handleInputChange}                     
-                        name="state" 
+                        onChange={this.handleInputChange}
+                        name="state"
                         placeholder="State*" />
-                    <Input 
+                    <Input
                         value={this.state.country}
-                        onChange={this.handleInputChange}                         
-                        name="country" 
+                        onChange={this.handleInputChange}
+                        name="country"
                         placeholder="Country*" />
-                    <Input 
+                    <Input
                         value={this.state.zip}
-                        onChange={this.handleInputChange}                     
-                        name="zip" 
+                        onChange={this.handleInputChange}
+                        name="zip"
                         placeholder="Zip Code*" />
-                    <Input 
+                    <Input
                         value={this.state.phoneNumber}
-                        onChange={this.handleInputChange}                         
-                        name="phoneNumber" 
+                        onChange={this.handleInputChange}
+                        name="phoneNumber"
                         placeholder="Phone Number*" />
-                    <Input 
+                    <Input
                         value={this.state.email}
-                        onChange={this.handleInputChange}                     
-                        name="email" 
+                        onChange={this.handleInputChange}
+                        name="email"
                         placeholder="E-Mail Address*" />
                     <span><i>*required field</i></span>
                     <FormBtn
-                        disabled={!(this.state.author &&
-                                    this.state.title &&
-                                    )}
+                        disabled={!(this.state.orgName &&
+                            this.state.website &&
+                            this.state.address1 &&
+                            this.state.city &&
+                            this.state.state &&
+                            this.state.country &&
+                            this.state.zip &&
+                            this.state.phoneNumber &&
+                            this.state.email)}
                         onClick={this.handleFormSubmit}>
-
                         Register Organization
                     </FormBtn>
+                    <SweetAlert
+                        show = {this.state.confirmSignUp}
+                        type = 'success'
+                        title = 'Registration Complete!'
+                        text = 'Your organization registration is completed. Thank you!'
+                        onConfirm = {() => this.setState({
+                            confirmSignUp: false
+                        })}
+                    />
                 </form>
             </div>
         );
