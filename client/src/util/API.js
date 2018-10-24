870f525fe4f73ec9c59b96c121d4d67f9252a6f8
b4ruch <12469509+b4ruch@users.noreply.github.com>
2018-10-23 21:07:44 -0700

    implements npo registration IU

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -2,12 +2,18 @@ import axios from "axios";
 
 export default {
 
-  login: function(user) {
-    return axios.post("/api/user/login",user);
+  login: function (user) {
+    return axios.post("/api/user/login", user);
   },
 
-  user: function(user){
-    return axios.post("/api/user/signup",user)
+  user: function (user) {
+    return axios.post("/api/user/signup", user)
+  },
+
+  //Registers new organization
+  newOrganization: function (organization) {
+    // return axios.post("api/organization", organization);
+    return "ok".json({ ok: "ok" });
   }
- 
+
 };
