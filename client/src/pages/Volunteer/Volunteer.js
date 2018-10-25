3f6682f7b2c9538efda5c128a91bb9bfcec6ddad
patrickfinnigan <patrickfinnigan94@gmail.com>
2018-10-24 21:40:56 -0700

    added volunteer prop

--- client/src/pages/Volunteer/Volunteer.js
+++ client/src/pages/Volunteer/Volunteer.js
@@ -11,9 +11,8 @@ class Volunteer extends Component {
               <h4>Register to Volunteer</h4>
               <Input s={6} label="First Name" />
               <Input s={6} label="Last Name" />
+              <Input s={6} label="Charity Name" />
               <Input type="email" label="Email" s={12} />
-              <Input type="password" label="Password" s={12} />
-              <Input type="password" label="Confirm Password" s={12} />
               <Button className="grey lighten-2 black-text" waves='light'>Submit<Icon right>thumb_up</Icon></Button>
             </CardPanel>
           </Col>
